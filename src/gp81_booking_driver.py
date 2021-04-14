070cebb9d941a33d553870c3a270aad66f894593
Larry Li <larry9607@gmail.com>
2021-04-14 10:45:32 -0400

    added wednesday noon process

--- src/gp81_booking_driver.py
+++ src/gp81_booking_driver.py
@@ -2,24 +2,34 @@
 
 """
 
+import argparse
 import configparser
+import datetime
 import logging
+import time
 
 from selenium import webdriver
 
 from src import gp81_flexbooker
 
-_config_file = './config.ini'
-
 
 def main():
+    ap = argparse.ArgumentParser(description='gp81 booker')
+    ap.add_argument('--wednesday_noon', action='store_true',
+                    help='flag to wait till 1 sec past 12 on Wednesdays before any booking action')
+    ap.add_argument('--config_file', default='./config.ini',
+                    help='path to the config file')
+    ap.add_argument('--logging_level', default='INFO',
+                    help='choice of [CRITICAL, ERROR, WARNING, INFO, DEBUG], default: debug')
+    args = ap.parse_args()
+
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
-                        level='INFO')
+                        level=args.logging_level)
 
-    logging.info(f'setting up config from {_config_file}')
+    logging.info(f'setting up config from {args.config_file}')
     cfg = configparser.ConfigParser()
-    cfg.read(_config_file)
+    cfg.read(args.config_file)
 
     booking_targets = gp81_flexbooker.parse_booking_rule(cfg['booking']['rule'])
     logging.info(f'booking: {[gp81_flexbooker.booking_target_to_human_readable(x) for x in booking_targets]}')
@@ -28,12 +38,28 @@ def main():
     driver = webdriver.Chrome()
     driver.implicitly_wait(int(cfg['site']['implicit_wait_secs']))
 
+    if args.wednesday_noon:
+        if datetime.date.today().isoweekday() != 3:
+            logging.info('seems like it is not Wednesday and --wednesday_noon flag is set, no action taken')
+        else:
+            booking_go_time = datetime.datetime(1, 1, 1, 12, 0, 1).time()
+            sleep_interval = float(cfg['booking']['wednesday_noon_sleep_wait_interval'])
+            while 1:
+                if datetime.datetime.now().time() > booking_go_time:
+                    logging.info('it\'s noon, starting booking process')
+                    break
+                else:
+                    logging.info(f'current time is {datetime.datetime.now()}, booking go time is {booking_go_time},'
+                                 f' sleeping {sleep_interval} secs')
+                    time.sleep(sleep_interval)
+
     try:
         gp81_flexbooker.login_and_go_to_calendar(driver, cfg)
         for target in booking_targets:
-            gp81_flexbooker.book(driver, cfg, target)
-    except Exception as e:
-        logging.exception(e)
+            try:
+                gp81_flexbooker.book(driver, cfg, target)
+            except Exception as e:
+                logging.error(f'failed to book {gp81_flexbooker.booking_target_to_human_readable(target)}: {e}')
     finally:
         logging.info('shutting down driver')
         driver.close()
